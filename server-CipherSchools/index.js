e6d59874fd8285bcf56af2b08480ea7fa9ded93e
MORSHED-HOME <morshed952640@gmail.com>
2023-03-10 22:59:55 +0600

    video play in local server

--- server-CipherSchools/index.js
+++ server-CipherSchools/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello CipherSchools!");
+});
+
+app.get("/video", (req, res) => {
+  res.sendFile("/assets/1.mp4", { root: __dirname });
 });
 
 app.listen(port, () => {
